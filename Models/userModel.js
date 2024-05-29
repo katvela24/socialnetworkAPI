4f50ac88227e84acf7716b35e70d59c10ac3f812
katvela24 <156866399+katvela24@users.noreply.github.com>
2024-05-29 12:54:22 -0400

    created the user Schema

---
+++ Models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+const emailRegex =  /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+
+// Creating the Schema for user.
+const userSchema = new Schema({
+    username: {
+        type: String, 
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String, 
+        unique: true,
+        required: true,
+        match: [emailRegex, "Please provide a valid email"],
+    },
+    thoughts: [{
+        type: mongoose.Schema.Types.ObjectId
+    }],
+
+    friends: [{
+        type: mongoose.Schema.Types.ObjectId
+    }]
+
+})
+// Creating a model with the Schema and labeling it as "User"
+const userModel = mongoose.model("User",userSchema)
+module.exports = userModel
